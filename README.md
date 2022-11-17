26c7e16d09ccea0c95a477178d64c19efc30a5cf
ParkShinwoo <myhappyman@naver.com>
2022-11-17 12:57:48 +0900

    animatePresence를 활용하여 슬라이더를 만들어보았다. 이전버튼까지 만들긴 했지만, 제대로 동작하지 않는다. 수정해볼 예정

--- README.md
+++ README.md
@@ -447,3 +447,65 @@ function App() {
   );
 }
 ```
+
+# 12. Slide part One
+
+AnimatePresence를 활용하여 slide효과를 만들어본다.
+
+```JSX
+import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+const Wrapper = styled(motion.div)`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  position:absolute;
+  top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #000;
+  font-size: 28px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  invisible: { x: 500, opacity: 0, scale: 0},
+  visible: { x:0, opacity: 1, scale: 1, transition: { duration: .5 }},
+  exit: { x: -500, opacity: 0, scale: 0, rotateX: 180, transition: { duration: .5 } },
+}
+
+function App() {
+  const [visible, setVisible] = useState(1);
+  const onNext = () => {
+    setVisible(prev => prev === 10 ? prev : prev+1);
+  };
+
+  return (
+    <Wrapper>
+      <AnimatePresence>
+        {[1,2,3,4,5,6,7,8,9,10].map(i =>
+          visible === i ?
+          <Box variants={boxVariants} initial="invisible" animate="visible" exit="exit" key={i}>{i}</Box>
+          : null)
+        }
+      </AnimatePresence>
+      <button onClick={onNext}>Next</button>
+    </Wrapper>
+  );
+}
+
+export default App;
+```
--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
@@ -15,43 +16,61 @@ const Box = styled(motion.div)`
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
+  position:absolute;
+  top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #000;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
-  initial: {
-    opacity:0,
+  invisible: {
+    x: 500,
+    opacity: 0,
     scale: 0
   },
   visible: {
-    opacity:1,
-    scale:1,
-    rotateZ: 360
+    x:0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: .5
+    }
   },
-  leaving: {
-    opacity:0,
-    scale:0,
-    y:10
+  exit: {
+    x: -500,
+    opacity: 0,
+    scale: 0,
+    rotateX: 180,
+    transition: {
+      duration: .5
+    }
   }
 }
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing(prev => !prev);
-
+  const [visible, setVisible] = useState(1);
+  const onNext = () => {
+    setVisible(prev => prev === 10 ? prev : prev+1);    
+  };     
+  const onPrev = () => {
+    setVisible(prev => prev === 1 ? prev : prev-1);    
+  };     
+  
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
-      <AnimatePresence>{showing ? <Box 
-                                    variants={boxVariants} 
-                                    initial="initial" 
-                                    animate="visible"
-                                    exit="leaving"
-                                  /> 
-                                : null}</AnimatePresence>
-        
+      <AnimatePresence>
+        {[1,2,3,4,5,6,7,8,9,10].map(i => 
+          visible === i ? 
+          <Box variants={boxVariants} initial="invisible" animate="visible" exit="exit" key={i}>{i}</Box> 
+          : null)
+        }
+      </AnimatePresence>
+      <button onClick={onPrev}>Prev</button>
+      <button onClick={onNext}>Next</button>
     </Wrapper>
   );
 }
